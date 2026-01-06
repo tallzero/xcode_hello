bb77e8c1ccc9bf50c0fefb984eb3e125d5fdd18f
tallzero <tallzero@gmail.com>
2026-01-06 18:13:17 +0900

    add Hello Earth to hotfix

--- git_xcode/main.c
+++ git_xcode/main.c
@@ -14,5 +14,6 @@ int main(int argc, const char * argv[]) {
     printf("Hello, Miku");
     printf("World, WRYYYYYYY!!!");
     printf("Hello, Luna");
+    printf("Hello, Earth");
     return EXIT_SUCCESS;
 }
