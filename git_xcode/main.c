ce3488b32d9695cb708313a4f1b6cee53063f58f
tallzero <tallzero@gmail.com>
2026-01-06 17:43:04 +0900

    Initial Commit

---
+++ git_xcode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  git_xcode
+//
+//  Created by Taeyoung Kim on 1/6/26.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
