2fda0e8fb8da5df69affdeb0e05d33010048875d
tallzero <tallzero@gmail.com>
2026-01-06 17:55:44 +0900

    hotfix changed

--- git_xcode/main.c
+++ git_xcode/main.c
@@ -12,5 +12,6 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
     printf("Hello, Miku");
+    printf("World, WRYYYYYYY!!!");
     return EXIT_SUCCESS;
 }
