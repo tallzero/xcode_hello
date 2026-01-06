6ab6edc0e942a08a9c32c158433de0d13942a23a
tallzero <tallzero@gmail.com>
2026-01-06 18:24:02 +0900

    added Conflict hotfix

--- git_xcode/main.c
+++ git_xcode/main.c
@@ -15,5 +15,6 @@ int main(int argc, const char * argv[]) {
     printf("World, WRYYYYYYY!!!");
     printf("Hello, Luna");
     printf("Hello, Earth");
+    printf("Conflict hotfix");
     return EXIT_SUCCESS;
 }
